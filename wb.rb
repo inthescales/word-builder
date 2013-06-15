ff62cb2329a9a63f2dac74bd6b8889d6bd066276
Robin Hill <rehill012@gmail.com>
2013-06-15 18:04:31 -0400

    - Massive cleanup. Less separation between words and affixes. Error checking happens before processing. No bugs found yet...

--- wb.rb
+++ wb.rb
@@ -14,230 +14,60 @@ def import_language(lang)
 	@affixes= JSON.parse(IO.read(lang+".affixes"));
 end
 
-# Utilities:
+# Utilities
 def is_vowel(letter)
 	
 	letter.downcase;
 	case letter
-		when 'a' then return true;
-		when 'e' then return true;
-		when 'i' then return true;
-		when 'o' then return true;
-		when 'u' then return true;
+		when 'a' then return true
+		when 'e' then return true
+		when 'i' then return true
+		when 'o' then return true
+		when 'u' then return true
 		else return false
 	end
 end
 
 def is_consonant(letter)
-	return !is_vowel(letter);
+	return !is_vowel(letter)
 end
 
-def is_content(word)
-	case word["category"]
-		when "noun" then return true;
-		when "adj" then return true;
-		when "verb" then return true;
+def is_word(token)
+	return token.index("-") == nil
+end
+
+def is_suffix(token)
+	return !is_word(token)
+end
+
+def is_free(token)
+	case token["category"]
+		when "noun" then return true
+		when "adj" then return true
+		when "verb" then return true
 		else return false
 	end
 end
 
-# Main
-def generate(input)
-	parsed = JSON.parse(input);
-	lang = parsed["language"];
-	output = []
-	cur_cat = nil;
-	
-	for i in 0..parsed["words"].length-1
-	
-		current = "";
-		entries = []
-		for j in 0..parsed["words"][i].length-1
-		
-			add = "";
-			comp = parsed["words"][i][j];
-			
-			if comp.index('-') == nil
-				# The component is a free morpheme
-				entry = @dictionary[comp]
-				entries[j] = entry;
-				
-				# Check the dictionary for this word
-				if entry == nil
-					print "ERROR: word \"", comp, "\" not found."
-					continue
-				end
-
-				# A boring old 'o' links content words. This will probably change
-				if is_content(entry) and j > 0 and is_content(entries[j-1])
-					current += "o"
-				end
-				
-				# If it doesn't assimilate, use link form
-				if entry["assimilation"] == nil
-					add = entry["link"]
-					
-				elsif j < parsed["words"][i].length-1
-					# Assimilate to fit with the next sound if necessary
-					assim = entry["assimilation"]
-					next_letter = @dictionary[parsed["words"][i][j+1]]["link"][0]
-					
-					assim.each do |type|
-						print next_letter, "\n"
-						print type, "\n"
-						if type[1].include?(next_letter) or type[1].include?("*")
-							
-							if type[0] == "link"
-								# use link form
-								add = entry["link"]
-								break
-								
-							elsif type[0] == "base"
-								# use base form
-								add = entry["base"]
-								break
-								
-							elsif type[0] == "nasal"
-								# use a nasal that fits with following sound
-								if next_letter == 'm' or next_letter == 'p' or next_letter == 'b'
-									add = entry["link"] + 'm';
-								else
-									add = entry["link"] + 'n';
-								end
-								break
-								
-							elsif type[0] == "double"
-								# duplicate following consonant
-								add = entry["link"] + next_letter
-								break
-
-							elsif type[0] == "cut"
-								# remove next letter
-								add = entry["base"] + "-"
-								break
-								
-							else
-								# custom assimilation - replace
-								add += type[0]
-								
-							end
-						end
-					end
-				end
-				cur_cat = entry["category"]
-				
-			else
-				# The component is an affix
-				entry = @affixes[comp]
-				entries[j] = entry;
-				
-				# Error handling
-				bad = false
-				if entry == nil
-					print "ERROR: affix \"", comp, "\" not found in language \"", lang, "\"\n";
-					bad = true
-				end
-
-				if cur_cat != entry["from"]
-					print "ERROR: cannot use affix \"", comp, "\" after \"", cur_cat, "\".";
-					bad = true
-				end
-				
-				if entry["follows"] != nil
-					entry["follows"].each do |ending|
-						if j > 0 and entries[j-1]["base"][-ending.length..-1] != ending
-							print "ERROR: affix \"", comp, "\" must follow \"", entry["follows"], "\".\n"
-							print "(found #{entries[j-1]["base"][-ending.length..-1]})\n"
-							bad = true
-						end
-					end
-				end
-
-				if bad
-					j = parse["words"][i].length
-					continue
-				end
-
-				# Check for final spelling changes
-				if entry["tail-mutator"] != nil
-					for c in 0..entry["tail-mutator"].length-1
-						check = entry["tail-mutator"][c]
-						len = check["old"].length
-						
-						if current[-len..-1] == check["old"]
-							current = current[0..current.length-len-1] + check["new"]
-						end
-					end
-				end
-				
-				# Verb stem changes
-				if entry["stem-change"] != nil
-					if current[-1,1] == "i" then current += "e" end
-					if current[-1,1] == "u" then current += "e" end
-				end
-
-				# Check for padding
-				if entry["vowel-padding"] != nil and is_consonant(current[-1,1])
-					current += entry["vowel-padding"];
-				elsif entry["consonant-padding"] != nil and is_vowel(current[-1,1])
-					current += entry["consonant-padding"];
-				elsif entry["special-padding"] != nil
-					if entry["special-padding"] == "verbstem"
-						current += entries[j-1]["conj"]
-					end
-				end
-				
-				# Add the new component
-				if j == parsed["words"][i].length-1
-					add = entry["base"];
-				else
-					add = entry["link"];
-				end
-				
-				# handle consonant dissonance
-				if entry["consonant-dissonance"] != nil
-					for k in 0..current.length-1
-						last = ""
-						if entry["consonant-dissonance"].include?(current[k])
-							last = current[k]
-						end
-					end
-					arr = entry["consonant-dissonance"]
-					arr.delete(last)
-					add += arr[0]
-				end
-				
-				# Prune matching letters
-				count = 0
-				for count in 0..add.length
-					if add[count] != current[current.length - (count + 1)] then break end
-				end
-				
-				add = add[count..add.length-1]
-				
-				cur_cat = entry["to"];
-			end
-			
-			# handle assimilation that cuts off a letter (cut)
-			if current[-1,1] == "-"
-				current = current[0..current.length-2]
-				add = add[1, add.length-1]
-			end
-			
-			current += add;
-			print "so far: #{current}\n"
-		
-		end
-
-		current = spelling("english", current)
-
-		output[i] = current;
-	end
-	
-	return output;
+def is_bound(token)
+	return !is_free(token)
 end
 
-# apply spelling rules
+def get_entry(token)
+	if is_word(token)
+		return @dictionary[token]
+	else
+		return @affixes[token]
+	end
+end
+
+def get_category(token)
+	r = token["category"]
+	if r == nil then r = token["to"] end
+	return r
+end
+
+# Target language spelling rules
 def spelling(lang, word)
 
 	if lang == "english"
@@ -252,6 +82,202 @@ def spelling(lang, word)
 	end
 	
 	return word
+end
+
+# Generate the words
+def generate(input)
+	parsed = JSON.parse(input);
+	lang = parsed["language"];
+	output = []
+	cur_cat = nil;
+	
+	for i in 0..parsed["words"].length-1
+	
+		current = "";
+		entries = []
+		
+		# Get entries and check for errors
+		for j in 0..parsed["words"][i].length-1
+			token = parsed["words"][i][j]
+			entries[j] = get_entry(token)
+			entry = entries[j]
+			ok = true
+			
+			if entry == nil
+				print "ERROR: token \"", token, "\" not found."
+				ok = false
+			end
+
+			if j > 0 and entry["from"] != nil and get_category(entries[j-1]) != entry["from"]
+				print "ERROR: cannot use affix \"", comp, "\" following \"", get_category(entries[j-1]), "s\".";
+				ok = false
+			end
+			
+			if entry["follows"] != nil
+				entry["follows"].each do |ending|
+					if j > 0 and entries[j-1]["base"][-ending.length..-1] != ending
+						print "ERROR: affix \"", comp, "\" must follow \"", entry["follows"], "\".\n"
+						print "(found #{entries[j-1]["base"][-ending.length..-1]}) instead\n"
+						ok = false
+					end
+				end
+			end
+			
+			if ok == false
+				output[i] = "nil"
+				break
+			end
+		end
+		
+		# If there was an error, skip this word
+		if output[i] == "nil" then continue end
+		
+		# Create the word
+		for j in 0..parsed["words"][i].length-1
+		
+			# set up some terms
+			add = "";
+			comp = parsed["words"][i][j];
+			next_comp = parsed["words"][i][j+1]
+			prev_comp = parsed["words"][i][j-1]
+			entry = entries[j]
+			next_entry = entries[j+1]
+			prev_entry = entries[j-1]
+			
+			# Make changes to the word so far ------------
+
+			# Check for final spelling changes
+			if entry["tail-mutator"] != nil
+				for c in 0..entry["tail-mutator"].length-1
+					check = entry["tail-mutator"][c]
+					len = check["old"].length
+					
+					if current[-len..-1] == check["old"]
+						current = current[0..current.length-len-1] + check["new"]
+					end
+				end
+			end
+			
+			# Verb stem changes
+			if entry["stem-change"] != nil
+				if current[-1,1] == "i" then current += "e" end
+				if current[-1,1] == "u" then current += "e" end
+			end
+			
+			# A boring old 'o' links content words. This will probably change
+			if is_free(entry) and j > 0 and is_free(prev_entry)
+				current += "o"
+			end
+			
+			# Make changes to the new part -------------
+			
+			# handle cut assimilation or other letter-removers
+			if current[-1,1] == "-"
+				current = current[0..current.length-2]
+				add = add[1, add.length-1]
+			end
+			
+			# Check for padding
+			if entry["vowel-padding"] != nil and is_consonant(current[-1,1])
+				add += entry["vowel-padding"];
+			elsif entry["consonant-padding"] != nil and is_vowel(current[-1,1])
+				add += entry["consonant-padding"];
+			elsif entry["special-padding"] != nil
+				if entry["special-padding"] == "verbstem"
+					add += entries[j-1]["conj"]
+				end
+			end
+			
+			# If it doesn't assimilate, use base or link as appropriate
+			if entry["assimilation"] == nil
+				if j == parsed["words"][i].length-1
+					add += entry["base"];
+				else
+					add += entry["link"];
+				end
+				
+			# Assimilate to fit with the next sound if necessary
+			elsif j < entries.length-1
+				assim = entry["assimilation"]
+				nesxt_letter = next_entry[0]
+				
+				assim.each do |type|
+					# Check each type of assimilation this entry uses until we find one that matches
+					# the next letter
+					if type[1].include?(next_letter) or type[1].include?("*")
+						
+						# use link form
+						if type[0] == "link"
+							add = entry["link"]
+							break
+							
+						# use base form
+						elsif type[0] == "base"
+							add = entry["base"]
+							break
+						
+						# use a nasal that fits with following sound
+						elsif type[0] == "nasal"
+							
+							if next_letter == 'm' or next_letter == 'p' or next_letter == 'b'
+								add = entry["link"] + 'm';
+							else
+								add = entry["link"] + 'n';
+							end
+							break
+							
+						# duplicate following consonant
+						elsif type[0] == "double"
+							add = entry["link"] + next_letter
+							break
+
+						# remove next letter
+						elsif type[0] == "cut"
+							add = entry["base"] + "-"
+							break
+							
+						# replace with a specified alternative
+						else
+							add += type[0]
+							
+						end
+					end
+				end
+			end
+			
+			# handle consonant dissonance
+			if entry["consonant-dissonance"] != nil
+				for k in 0..current.length-1
+					last = ""
+					if entry["consonant-dissonance"].include?(current[k])
+						last = current[k]
+					end
+				end
+				arr = entry["consonant-dissonance"]
+				arr.delete(last)
+				add += arr[0]
+			end
+			
+			# Prune matching letters
+			count = 0
+			for count in 0..add.length
+				if add[count] != current[current.length - (count + 1)] then break end
+			end
+			
+			add = add[count..add.length-1]
+			
+			current += add;
+			cur_cat = get_category(entry)
+			print "so far: #{current}\n"
+		end
+
+		# Apply target language spelling rules and save
+		current = spelling("english", current)
+
+		output[i] = current;
+	end
+	
+	return output;
 end
 
 # turn a list of words into a json query
